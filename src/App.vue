88e86310fd5da6cf7a605a76da274eda52f2746e
ronaldo martins <ronaldooracle@gmail.com>
2022-02-21 17:10:17 -0300

    #criação porjeto imc ronaldo

---
+++ src/App.vue
@@ -0,0 +1,84 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container>
+        <v-card elevation="19" loading>
+          <v-card-actions>IMC</v-card-actions>
+           <v-card-actions>Índice de Massa Corporal</v-card-actions>
+          <v-card-actions v-if="total != 0">Resultado: {{ total }}</v-card-actions>
+          <v-card-actions v-if="total != 0">Classificação: {{ status }}</v-card-actions>
+          <v-text-field
+            v-model="altura"
+            label="Altura"
+            filled
+            placeholder="1.75"
+          ></v-text-field>
+          <v-text-field
+            v-model="peso"
+            label="Peso"
+            filled
+            placeholder="85.0"
+          ></v-text-field>
+          <v-btn
+            color="success"
+            class="ma-2 white--text"
+            @click="Calcular"
+            >Calcular</v-btn
+          >
+        </v-card>
+      </v-container>
+    </v-main>
+    <Footer />
+  </v-app>
+</template>
+<script>
+import Footer from './components/Footer.vue';
+export default {
+  components: { Footer },
+  data() {
+    return {
+      altura: 0,
+      peso: 0,
+      total: 0,
+      status: "",
+    };
+  },
+  methods: {
+    Calcular() {
+      if (this.altura && this.peso > 0) {
+        this.total = this.peso / this.altura ** 2;
+        if (this.total < 18.05) this.status = "Abaixo do Peso";
+        else if (this.total < 24.09) this.status = "Saudavel";
+        else if (this.total < 25.09) this.status = "Peso Excesso";
+        else if (this.total < 30.0) this.status = "Obesidade Grau I";
+        else if (this.total < 35.0) this.status = "Obesidade Grau II";
+        else if (this.total >= 40.0) this.status = "Obesidade Grau III";
+      }
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background-color: aliceblue;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-footer
+    color="primary lighten-1"
+    padless
+  >
+    <v-row
+      justify="center"
+      no-gutters
+    >
+      <v-btn
+        v-for="link in links"
+        :key="link"
+        color="white"
+        text
+        rounded
+        class="my-2"
+        to="\www.linkedin.com/in/ronaldo-martins-da-silva-a07131122/"
+        target="_blank"
+      >
+        {{ link }}
+      </v-btn>
+      <v-col
+        class="primary lighten-2 py-4 text-center white--text"
+        cols="12"
+      >
+        {{ new Date().getFullYear() }} — <strong>Ronaldo Martins</strong>
+      </v-col>
+    </v-row>
+  </v-footer>
+</template>
+<script>
+  export default {
+    data: () => ({
+      links: [
+        'GitHub',
+        'Sobre',
+        'Linkdin',
+        'Services',
+        'Blog',
+        'Fone',
+      ],
+    }),
+  }
+</script>
